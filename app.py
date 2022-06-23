3558ffe0ffcc459766a2477b908424b01ff67cc0
KrishnaTeja-97 <krishnateja9014@gmail.com>
2022-06-24 05:17:49 +0530

    created somepages

--- app.py
+++ app.py
@@ -4,25 +4,6 @@ from flask import request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def get_index():
-    return "<p>Hello from an extremely interesting web page!</p>"
-
-@app.route("/hello")
-def get_hello():
-    return render_template('hello.html', name="Greg")
-
-@app.route("/santa")
-def get_santa():
-    toy = request.args.get("toy","pony")
-    return render_template('hello.html', name="Santa is sending a " + toy + "!")
-
-@app.route("/hi")
-@app.route("/hi/")
-@app.route("/hi/<name>")
-def get_hi(name="Dorothy"):
-    return render_template('hello.html', name=name)
-
 @app.route("/login", methods=['GET'])
 def get_login():
     return render_template('login.html')
@@ -31,7 +12,15 @@ def get_login():
 def post_login():
     username = request.form.get("username", "<missing name>")
     password = request.form.get("password", "<missing password>")
-    if password == "password1":
-        return redirect(url_for('get_hi', name=username))
-    else:
-        return redirect(url_for('get_login'))
+
+@app.route("/Register", methods=['GET'])
+def get_register():
+    return render_template('register.html')
+
+@app.route("/Register", methods=['POST'])
+def post_register():
+    username = request.form.get("username", "<missing name>")
+    password = request.form.get("password", "<missing password>")
+    repeat_password = request.form.get("repeat_password", "<missing password>")
+
+
